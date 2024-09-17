6b626ef49afddd21c4454871cce6eebf3235f5aa
Abhinav <abhinav@AVs-Mac.local>
2024-09-17 23:24:09 +0530

    [17th sep 2024] npm and node js modules

---
+++ npm_modules/app.js
@@ -0,0 +1,17 @@
+/*command: node 
+REPL --> Read, Evaluate, Print, Loop
+*/
+
+console.log("starting be_aug_2023");
+var a = 10;
+var b = a+10;
+console.log(a+b);
+
+export const add = (a,b) => {
+    return a+b
+}
+
+export const subtract = (a,b) => {
+    return a-b
+}
+
